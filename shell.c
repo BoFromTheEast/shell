d503afdfcb6a9d4d4f4fe3c480b116d7d3bd8997
Bo Oo <bhoo@iastate.edu>
2025-08-19 00:42:40 -0500

    Implemented readingline feature

--- shell.c
+++ shell.c
@@ -1,7 +1,74 @@
 #include <stdio.h>
 
+int main(int argc, char *argv)
+{
 
+    shellLoop();
+}
 
-int main(int argc, char* argv){
+void shellLoop(void)
+{
+    char *line;
+    char **args;
+    int loopStatus;
 
+    do
+    {
+        printf("> ");
+        line = shell_readline();
+        args = shell_splitline(line);
+        loopStatus = shell_execute(args);
+
+        free(line);
+        free(args);
+    } while (loopStatus);
+}
+
+char *shell_readline(void)
+{
+    int bufferSize = 1024;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * bufferSize);
+    int c;
+
+    if (!buffer)
+    {
+        fprintf(stderr, "Buffer allocation failed");
+        EXIT();
+    }
+
+    while (1)
+    {
+
+        c = getchar();
+
+        if (c == EOF || c == "\n")
+        {
+            buffer[position] = '\0';
+        }
+        else
+        {
+            buffer[position] = c;
+        }
+        position++;
+
+        if (position >= bufferSize)
+        {
+            bufferSize += bufferSize;
+            realloc(buffer, bufferSize);
+            if (!buffer)
+            {
+                fprintf(stderr, "Buffer allocation failed");
+                EXIT();
+            }
+        }
+    }
+}
+
+char **shell_splitline(char **line)
+{
+}
+
+int shell_execute(char ***args)
+{
 }
